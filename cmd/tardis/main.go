3747efa451ab4fa90095abfe6fcffd8cec21f733
Håkon Solbjørg <hakon@solbj.org>
2020-09-29 15:30:57 +0200

    feat: Add DevMode

--- cmd/tardis/main.go
+++ cmd/tardis/main.go
@@ -8,21 +8,30 @@ import (
 	"syscall"
 
 	"github.com/bwmarrin/discordgo"
+	log "github.com/sirupsen/logrus"
 	"github.com/sklirg/tardis/hots"
 )
 
 type tardis struct {
-	AramBuilds *hots.AramBuilds
+	AramBuilds       *hots.AramBuilds
+	DevMode          bool
+	DevListenChannel string
 }
 
 func main() {
 	discordBotToken := os.Getenv("TARDIS_DISCORD_TOKEN")
 
 	state := tardis{
+		DevMode: os.Getenv("TARDIS_DEV") != "",
 		AramBuilds: &hots.AramBuilds{
 			SheetID:    os.Getenv("TARDIS_HOTS_ARAM_SHEET_ID"),
 			SheetRange: os.Getenv("TARDIS_HOTS_ARAM_SHEET_RANGE"),
 		},
+	}
+
+	if state.DevMode {
+		log.Info("Starting in dev mode")
+		log.SetLevel(log.TraceLevel)
 	}
 
 	dg, err := discordConnect(discordBotToken)
@@ -39,10 +48,12 @@ func main() {
 		return
 	}
 
-	fmt.Println("Bot is now running. Press CTRL-C to exit.")
+	log.Info("Bot is now running. Press CTRL-C to exit.")
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
 	<-sc
+
+	log.Info("Received interrupt, shutting down.")
 
 	dg.Close()
 }
@@ -61,6 +72,10 @@ func (tardis *tardis) messageCreate(s *discordgo.Session, m *discordgo.MessageCr
 	if m.Author.ID == s.State.SessionID {
 		return
 	}
+	logger := log.WithFields(log.Fields{
+		"author_id": m.Author.ID,
+		"author":    m.Author.String(),
+	})
 
 	if len(m.Content) == 0 {
 		return
@@ -76,10 +91,34 @@ func (tardis *tardis) messageCreate(s *discordgo.Session, m *discordgo.MessageCr
 		return
 	}
 
-	switch tokens[0] {
+	trigger := tokens[0]
+
+	if tardis.DevMode && !(trigger == "listen" || m.ChannelID == tardis.DevListenChannel) {
+		// In DevMode and received message in a channel I don't listen to, so skip
+		// But will allow the keyword 'listen' through
+		return
+	}
+	logger = logger.WithField("trigger", trigger)
+
+	switch trigger {
 	case "aram", "hots":
 		{
 			tardis.AramBuilds.HandleDiscordMessage(s, m)
+		}
+	case "listen":
+		{
+			if !tardis.DevMode {
+				// If we receive the `listen` trigger while not in DevMode we don't care
+				return
+			}
+			tardis.DevListenChannel = m.ChannelID
+			s.ChannelMessageSend(m.ChannelID, ":robot: :construction: Listening to this channel")
+		}
+	default:
+		{
+			if tardis.DevMode {
+				logger.Debug("Received unknown trigger")
+			}
 		}
 	}
 }
--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.13
 
 require (
 	github.com/bwmarrin/discordgo v0.20.2
+	github.com/sirupsen/logrus v1.7.0
 	google.golang.org/api v0.17.0
 )
