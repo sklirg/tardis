0aa71068600bbebb9a87240268e9ee6f479e3ea7
Håkon Solbjørg <hakon@solbj.org>
2021-05-06 17:08:22 +0200

    fix: Allow bot to work without database connection

--- server/db.go
+++ server/db.go
@@ -1,7 +1,7 @@
 package server
 
 import (
-	//"github.com/bwmarrin/discordgo"
+	"errors"
 	log "github.com/sirupsen/logrus"
     // Imported for side effects
 	_ "github.com/lib/pq"
@@ -27,6 +27,11 @@ type WelcomeChannel struct {
 }
 
 func (srv *DiscordServerStore) StoreReactRole(rr ReactRole) error {
+    if db == nil {
+        log.Error("Database is nil!")
+        return errors.New("not connected to database")
+    }
+
     log.Debug("Inserting ReactRoleMessage in DB")
 	_, err := db.Query("INSERT INTO reaction_messages (guild, channel, id) VALUES ($1, $2, $3) ON CONFLICT DO NOTHING", rr.Message.GuildID, rr.Message.ChannelID, rr.Message.ID)
     if err != nil {
@@ -45,6 +50,11 @@ func (srv *DiscordServerStore) StoreReactRole(rr ReactRole) error {
 }
 
 func (srv *DiscordServerStore) GetReactRolesForMessage(rm ReactRoleMessage) ([]*ReactRole, error) {
+    if db == nil {
+        log.Error("Database is nil!")
+        return nil, errors.New("not connected to database")
+    }
+
     rows, err := db.Query("SELECT id, reaction, role FROM reaction_message_reactions WHERE message_guild = $1 AND message_channel = $2 AND message_id = $3", rm.GuildID, rm.ChannelID, rm.ID)
     if err != nil {
         log.WithError(err).Error("Failed to SELECT")
@@ -69,6 +79,11 @@ func (srv *DiscordServerStore) GetReactRolesForMessage(rm ReactRoleMessage) ([]*
 }
 
 func (srv *DiscordServerStore) StoreWelcomeChannel(w WelcomeChannel) error {
+    if db == nil {
+        log.Error("Database is nil!")
+        return errors.New("not connected to database")
+    }
+
     log.Debug("Inserting Welcome Channel in DB")
 	_, err := db.Query("INSERT INTO welcome_channel (guild, message_channel, emoji_channel) VALUES ($1, $2, $3) ON CONFLICT (guild) DO UPDATE SET message_channel = $2", w.GuildID, w.MessageChannelID, w.EmojiChannelID)
     if err != nil {
@@ -80,6 +95,11 @@ func (srv *DiscordServerStore) StoreWelcomeChannel(w WelcomeChannel) error {
 }
 
 func (srv *DiscordServerStore) GetWelcomeChannel(guildID string) (*WelcomeChannel, error) {
+    if db == nil {
+        log.Error("Database is nil!")
+        return nil, errors.New("not connected to database")
+    }
+
     log.Debug("Fetching welcome channel from DB")
     rows, err := db.Query("SELECT guild, message_channel, emoji_channel FROM welcome_channel WHERE guild = $1", guildID)
     if err != nil {
